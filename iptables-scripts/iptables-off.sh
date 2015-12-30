10b33cfb1aceb23c674e06a7133a5e58e6163c27
Jérémie Decock <jd.jdhp@gmail.com>
2015-12-30 15:38:30 +0100

    Add the copyright notice and some comments.

--- iptables-scripts/iptables-off.sh
+++ iptables-scripts/iptables-off.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
+# Copyright (c) 2007,2015 Jérémie DECOCK <jd.jdhp@gmail.com> (www.jdhp.org)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# For more information, see https://github.com/jeremiedecock/iptables-scripts
+
 IP4TABLES="/sbin/iptables -v"
 IP6TABLES="/sbin/ip6tables -v"
 
+# Setup Netfilter for IPv4 and IPv6
 for IPTABLES in "$IP4TABLES" "$IP6TABLES"
 do
+    # Flush rules
     $IPTABLES -F
     $IPTABLES -X
 
+    # Default rule: accept every packet (incoming, outgoing and forwarded)
     $IPTABLES -P INPUT   ACCEPT
     $IPTABLES -P OUTPUT  ACCEPT
     $IPTABLES -P FORWARD ACCEPT
--- iptables-scripts/iptables-paranoid.sh
+++ iptables-scripts/iptables-paranoid.sh
@@ -1,17 +1,43 @@
 #!/bin/sh
 
+# Copyright (c) 2007,2015 Jérémie DECOCK <jd.jdhp@gmail.com> (www.jdhp.org)
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# For more information, see https://github.com/jeremiedecock/iptables-scripts
+
 IP4TABLES="/sbin/iptables -v"
 IP6TABLES="/sbin/ip6tables -v"
 
+# Setup Netfilter for IPv4 and IPv6
 for IPTABLES in "$IP4TABLES" "$IP6TABLES"
 do
+    # Flush rules
     $IPTABLES -F
     $IPTABLES -X
 
+    # Default rule: silently reject every packet (incoming, outgoing and forwarded)
     $IPTABLES -P INPUT   DROP
     $IPTABLES -P OUTPUT  DROP
     $IPTABLES -P FORWARD DROP
 
+    # Accept every communication on the loopback interface
     $IPTABLES -A INPUT -i lo -j ACCEPT
     $IPTABLES -A OUTPUT -o lo -j ACCEPT
 done
